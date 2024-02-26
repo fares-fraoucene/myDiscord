e8df71005b1c28afc45d1f6654c8e009a75d43ba
fares-fraoucene <fares.fraoucene@laplateforme.io>
2024-02-26 03:07:31 +0100

    ajout d'une fonction permettant de créer un rectangle

--- Tools/shapes.py
+++ Tools/shapes.py
@@ -4,5 +4,7 @@ class Shapes:
     def __init__(self, display):
         self.display = display
         self.screen = self.display.screen
+    def draw_rect(self, x, y, width, height, color):
+        pygame.draw.rect(self.screen, color, (x, y, width, height))
 
     

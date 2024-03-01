49640480ec24c8125896203904e67266f352a3ad
fares-fraoucene <fares.fraoucene@laplateforme.io>
2024-03-01 17:20:23 +0100

    ajout de la focntion check user

--- Base_sql/check_user.py
+++ Base_sql/check_user.py
@@ -1,15 +1,15 @@
-# import mysql.connector
+import mysql.connector
 
-# def check_user(email, password):
-#     conn = mysql.connector.connect(host="localhost", user="root", password="toto", database="my_Discord")
-#     cursor = conn.cursor()
-#     query = "SELECT email, mot_de_passe FROM utilisateurs WHERE email = %s AND mot_de_passe = %s"
-#     cursor.execute(query, (email, password))
-#     user = cursor.fetchone()
-#     cursor.close()
-#     conn.close()
-#     if user:
-#         return True
-#     else:
-#         return False
+def check_user(email, password):
+    conn = mysql.connector.connect(host="localhost", user="root", password="toto", database="my_Discord")
+    cursor = conn.cursor()
+    query = "SELECT email, mot_de_passe FROM utilisateurs WHERE email = %s AND mot_de_passe = %s"
+    cursor.execute(query, (email, password))
+    user = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if user:
+        return True
+    else:
+        return False
 
